79cc601c6503d973d6322fca4164c3e8411efcbf
Tony Pai <tony77794@gmail.com>
2018-08-26 10:50:28 +0800

    Extract generateResponse

--- index.js
+++ index.js
@@ -2,40 +2,30 @@ require('dotenv').config();
 
 const { putItem } = require('./lib/aws');
 const verify = require('./lib/recaptcha');
+const generateResponse = require('./lib/generateResponse');
 
 const handler = async ({
   body,
   httpMethod,
   path,
 }) => {
+  console.log(httpMethod, path);
   if (httpMethod === 'POST' && path === process.env.API_GATEWAY_RESOURCE) {
     try {
       const { response, ...restProps } = JSON.parse(body);
       const { success } = await verify(response);
       if (success) {
         const json = await putItem(response, restProps);
-        return {
-          statusCode: '200',
-          body: 'success',
-        };
+        return generateResponse(200, 'success');
       } else {
-        return {
-          statusCode: '401',
-          body: 'unauthorized',
-        };
+        return generateResponse(401, 'unauthorized');
       }
     } catch (err) {
       console.log(err);
-      return {
-        statusCode: '400',
-        body: 'invalid request',
-      };
+      return generateResponse(400, 'invalid request');
     }
   }
-  return {
-    statusCode: '404',
-    body: 'Not Found',
-  };
+  return generateResponse(404, 'not found');
 };
 
 exports.handler = handler;
